f77e21ade7ab94c207f23870c445d05ba310a9f7
orlevi <orlevi1234@gmail.com>
2015-05-11 20:14:10 +0300

    still a bit of trying

---
+++ soundDemo/soundDemo/sound/gui.py
@@ -0,0 +1,17 @@
+'''
+Created on May 11 2015
+
+@author: Or Levi
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
