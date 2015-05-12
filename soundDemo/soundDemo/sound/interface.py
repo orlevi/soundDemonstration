6e82531d6ad6d4c1d7fdd3e8675721beaedd6644
omercoh <omercoh@gmail.com>
2015-05-13 00:00:43 +0300

    Basic Interface
    With volume and frequency

--- soundDemo/soundDemo/sound/interface.py
+++ soundDemo/soundDemo/sound/interface.py
@@ -3,3 +3,31 @@ Created: May 12, 2015
 
 @author: Omer Cohen
 '''
+
+class Interface():
+
+	def __init__(self, volume, frequency):
+
+		self.vol = volume
+		self.freq = frequency
+	def increaseVol(self):
+		self.vol += 1
+	def decreaseVol(self):
+
+		self.vol -=1
+
+	def increaseFreq(self, fine):
+
+		if fine:
+			self.freq += 0.5
+		else:
+			self.freq += 2
+	def decreaseFreq(self, fine):
+
+		if fine:
+
+			self.freq -= 0.5
+
+		else:
+
+			self.freq -= 2
