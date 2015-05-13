f0f0395df3f0fc33db790466d405349b7b524720
omercoh <omercoh@gmail.com>
2015-05-13 11:43:27 +0300

    Edited Interface

--- soundDemo/soundDemo/sound/interface.py
+++ soundDemo/soundDemo/sound/interface.py
@@ -1,33 +1,40 @@
 '''
-Created: May 12, 2015
-
-@author: Omer Cohen
+' Created: May 12, 2015
+'
+' @author: Omer Cohen
 '''
 
 class Interface():
 
 	def __init__(self, volume, frequency):
-
 		self.vol = volume
 		self.freq = frequency
+        finetune = 0.5
+        coarsetune = 2
+
+    def mute(self)
+        self.vol = 0
+        
 	def increaseVol(self):
 		self.vol += 1
-	def decreaseVol(self):
 
+	def decreaseVol(self):
 		self.vol -=1
 
-	def increaseFreq(self, fine):
-
+    def setTuneStep(self, val, fine=0)
+        if fine:
+            finetune = val
+        else
+            coarsetube = val
+            
+	def increaseFreq(self, fine=0):
 		if fine:
 			self.freq += 0.5
 		else:
 			self.freq += 2
-	def decreaseFreq(self, fine):
 
+	def decreaseFreq(self, fine=0):
 		if fine:
-
-			self.freq -= 0.5
-
+			self.freq -= finetune
 		else:
-
-			self.freq -= 2
+			self.freq -= coarsetune
